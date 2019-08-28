a33b6bb36d3361f17bb98495d7350cbfb42dd55c
rodri <rmzancs@gmail.com>
2019-08-28 11:47:47 -0300

    Commit Inicial Preparação de exemplos e cabeçalho c/ objetivos...

--- Ativ02.py
+++ Ativ02.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Nome: Rodrigo Malosti Zanco - RA: 205541
 # Atividade 02 - Extrair info de curso através de várias paginas HTML do catálogo.
 # Obter nome, créditos, ementa, pré-requisitos
